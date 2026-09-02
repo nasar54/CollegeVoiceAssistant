dce251aa7c9ffacf32bbbd7b463d5106bfcb73b2
nasar54 <n9959926@gmail.com>
2026-09-02 14:57:04 +0530

    Add files via upload

--- college_data.py
+++ college_data.py
@@ -3,7 +3,7 @@
 # =========================
 
 student_info = {
-    "name": "Chaithanya",
+    "name": "ch.Nasar",
     "course": "CSE - AI and ML",
     "year": "1st year"
 }
@@ -26,16 +26,16 @@ college_info = {
 # =========================
 
 timetable = {
-    "Tuesday": [
-        "8:30 AM - 9:20 AM — Course Overview",
-        "9:20 AM - 10:10 AM — Programming with Python MCQ Practice",
+    "wednesday": [
+        "8:30 AM - 9:20 AM — introduction to HTML|MCQ practise",
+        "9:20 AM - 10:10 AM — MCQ practice",
         "10:10 AM - 10:25 AM — Break",
-        "10:25 AM - 11:00 AM — Practice Session",
-        "11:00 AM - 11:50 AM — Introduction to GenAI in Web Development",
-        "11:50 AM - 12:40 PM — Coding Practice Walkthrough | Part 1",
+        "10:25 AM - 11:00 AM — variables and data types MCQ Practice ",
+        "11:00 AM - 11:50 AM — levereging gen ai for accelerated learning",
+        "11:50 AM - 12:40 PM — numbers Daily practice",
         "12:40 PM - 1:30 PM — Lunch Break",
-        "1:30 PM - 2:20 PM — Generative AI Foundations",
-        "2:20 PM - 3:10 PM — MCQ Practice",
+        "1:30 PM - 2:20 PM — productivity power up with ai tools",
+        "2:20 PM - 3:10 PM — knowledge v/s skill",
         "3:10 PM - 4:00 PM — Doubt Solving"
     ]
 }
